b072a65e9d14a5864811275d0e61904864adff83
AlphaTech <AlphaT3ch@icloud.com>
2015-05-30 19:56:12 -0400

    Added script

--- cat.php
+++ cat.php
@@ -1,5 +1,4 @@
 #!/usr/bin/php
 <?php
-$page = file_get_contents("http://thecatapi.com/api/images/get?format=src&results_per_page=1&type=jpg,gif,png&category=none&size=full&sub_id=none");
-echo $page;
+echo str_replace("\">", "", str_replace("<img src=\"", "", strip_tags(file_get_contents('http://thecatapi.com/api/images/get?format=html'), '<img></img>')));
 ?>
