a8f57a91466841c4411d5b4d448a4c84c73ea900
AlphaTech <alphat3ch@icloud.com>
2015-09-04 13:40:09 -0400

    Update global.sh

--- global.sh
+++ global.sh
@@ -5,16 +5,19 @@ then
 wget -O /usr/bin/catpic -q https://raw.githubusercontent.com/AlphaT3ch/TheCatAPI-Cli/master/cat.sh
 chmod +x /usr/bin/catpic
 echo "Successfully installed catpic to /usr/bin"
+echo "To uninstall, run \"rm /usr/bin/catpic\""
 elif [[ " ${paths[*]} " == *" /bin "* ]]
 then
 wget -O /bin/catpic -q https://raw.githubusercontent.com/AlphaT3ch/TheCatAPI-Cli/master/cat.sh
 chmod +x /bin/catpic
 echo "Successfully installed catpic to /bin"
+echo "To uninstall, run \"rm /bin/catpic\""
 elif [[ " ${paths[*]} " == *" /usr/sbin "* ]]
 then
 wget -O /usr/sbin/catpic -q https://raw.githubusercontent.com/AlphaT3ch/TheCatAPI-Cli/master/cat.sh
 chmod +x /usr/sbin/catpic
 echo "Successfully installed catpic to /usr/sbin"
+echo "To uninstall, run \"rm /usr/sbin/catpic\""
 else
 wget -O /usr/bin/catpic -q https://raw.githubusercontent.com/AlphaT3ch/TheCatAPI-Cli/master/cat.sh
 chmod +x /usr/bin/catpic
